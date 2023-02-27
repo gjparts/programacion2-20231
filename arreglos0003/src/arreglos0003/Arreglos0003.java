be65db65f82606a270e2e5305bd2c9ab1296f6a8
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-02-26 23:16:40 -0600

    commit 260220232679

--- arreglos0003/src/arreglos0003/Arreglos0003.java
+++ arreglos0003/src/arreglos0003/Arreglos0003.java
@@ -13,8 +13,8 @@ public class Arreglos0003 {
         5) Salir
         
         nota: los numeros al azar deberan ser entre 1 y 1000
-        Programar cada una de las opciones del menu, cuando se seleccione una opcion, el menu debera volver
-        a mostrarse*/
+        Programar cada una de las opciones del menu, cuando se seleccione una
+        opcion, el menu debera volver a mostrarse*/
         Scanner  s = new Scanner(System.in);
         int opcion;
         
@@ -28,7 +28,41 @@ public class Arreglos0003 {
             System.out.println("5) Salir");
             System.out.print("Digitar la opcion deseada: ");
             opcion = s.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    opcion1();  //mandar a llamar la funcion externa
+                break;
+                case 2:
+                    opcion2();  //mandar a llamar la funcion externa
+                break;
+                case 3:
+                    opcion3();  //mandar a llamar la funcion externa
+                break;
+                case 4:
+                    opcion4();  //mandar a llamar la funcion externa
+                break;
+                default:
+                    System.out.println("Opcion no valida.");
+            }
         }while( opcion != 5 );
-    }
+    }   //aqui se termina en main
     
+    //funciones
+    //funcion que ejecuta el codigo de la opcion 1 del menu
+    public static void opcion1(){
+        System.out.println("HA ELEGIDO LA OPCION 1");
+    }
+    //funcion que ejecuta el codigo de la opcion 2 del menu
+    public static void opcion2(){
+        System.out.println("HA ELEGIDO LA OPCION 2");
+    }
+    //funcion que ejecuta el codigo de la opcion 3 del menu
+    public static void opcion3(){
+        System.out.println("HA ELEGIDO LA OPCION 3");
+    }
+    //funcion que ejecuta el codigo de la opcion 4 del menu
+    public static void opcion4(){
+        System.out.println("HA ELEGIDO LA OPCION 4");
+    }
 }
